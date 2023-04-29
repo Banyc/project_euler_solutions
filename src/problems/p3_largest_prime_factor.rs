817b51dbd324ef2bd20e29e61deef3d28ce64a8c
Banyc <36535895+Banyc@users.noreply.github.com>
2023-04-29 23:29:09 +0800

    refactor: primality test algorithm

--- src/problems/p3_largest_prime_factor.rs
+++ src/problems/p3_largest_prime_factor.rs
@@ -24,13 +24,39 @@ pub fn largest_prime_factor(n: usize) -> Option<usize> {
     None
 }
 
-fn is_prime(n: usize) -> bool {
-    let max = n / 2;
-    for i in 2..max {
-        let other = n / i;
-        if other * i == n {
+/// # References
+///
+/// - <https://en.wikipedia.org/wiki/Primality_test>
+pub fn is_prime(n: usize) -> bool {
+    // n = 0, 1, 2, 3, 4, 5, 6, 7, ...
+    if n == 0 {
+        return false;
+    }
+    // n = 1, 2, 3, 4, 5, 6, 7, 8, ...
+    if n < 4 {
+        return true;
+    }
+    // n = 4, 5, 6, 7, 8, 9, 10, 11, ...
+    if n % 2 == 0 {
+        return false;
+    }
+    // n = 5, 7, 9, 11, 13, 15, 17, ...
+    if n % 3 == 0 {
+        return false;
+    }
+    // n = 5, 7, 11, 13, 17, 19, 23, ...
+    // `i` is a factor of `n`
+    let mut i = 5;
+    while i * i <= n {
+        // i     = 5,    11,     17,     23,     ..., sqrt(n)
+        // i + 2 =    7,     13,     19,     25, ...,         sqrt(n) + 2
+        if n % i == 0 {
             return false;
         }
+        if n % (i + 2) == 0 {
+            return false;
+        }
+        i += 6;
     }
     true
 }
